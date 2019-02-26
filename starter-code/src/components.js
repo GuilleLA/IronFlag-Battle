db75714840aa891645a1724d04df31c1841a6072
Guille LA <guillermolucena90@gmail.com>
2019-02-26 16:34:24 +0100

    creation of flag constructor

--- starter-code/src/components.js
+++ starter-code/src/components.js
@@ -1,11 +1,21 @@
 
 //Constructor of flags
-
+function Flags (width, height, x, y) {
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
+  this.draw = function() {
+    ctx = myGameArea.canvas.getContext("2d");
+    ctx.fillStyle = "blue";
+    ctx.fillRect(x, y, this.width, this.height);
+  }
+}
 
 
 //Constructor of killing balls
 function Component(width, height, x, y){
-  this.with = width;
+  this.width = width;
   this.height = height;
   this.x = x;
   this.y = y;
@@ -14,7 +24,7 @@ function Component(width, height, x, y){
   this.draw = function() {
     ctx = myGameArea.canvas.getContext("2d");
     ctx.beginPath();
-    ctx.arc(400, 400, 15, 0, Math.PI*2, true)
+    ctx.arc(this.x, this.y, 15, 0, Math.PI*2, true)
     ctx.fill();
   }
 }
--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -2,9 +2,13 @@ var mainImage = document.querySelector(".main-image");
 var selection = document.querySelector(".selection");
 
 window.onload = function() {
+  flagPT = new Flags(20, 20, 20, 20)
+  flagFT = new Flags(20, 20, 760, 760)
   document.getElementById("start-button").onclick = function() {
     mainImage.classList.add("hide");
     selection.classList.add("hide");
     myGameArea.start();
+    flagPT.draw();
+    flagFT.draw();
   };
 }
