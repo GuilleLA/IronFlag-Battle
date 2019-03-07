f655a45b16bbf3dc4a810fe67c5f4cf765294020
Guille LA <guillermolucena90@gmail.com>
2019-03-07 18:50:08 +0100

    added functionality to char sel buttons

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -5,10 +5,16 @@ var scorePtHtml = document.querySelector(".score-pt");
 var scoreFtHtml = document.querySelector(".score-ft");
 var finalScreen = document.querySelector(".final-screen");
 var winner = document.querySelector(".winner");
-var ptCollection = document.getElementsByClassName("playerPT");
-var ptArray = [].slice.call(ptCollection);
-var ftCollection = document.getElementsByClassName("playerFT");
-var ftArray = [].slice.call(ftCollection);
+var ptImgCollection = document.getElementsByClassName("playerPT");
+var ptImgArray = [].slice.call(ptImgCollection);
+var ptRoleCollection = document.getElementsByClassName("rolePT");
+var ptRoleArray = [].slice.call(ptRoleCollection);
+var ptArraycount = 0;
+var ftImgCollection = document.getElementsByClassName("playerFT");
+var ftImgArray = [].slice.call(ftImgCollection);
+var ftRoleCollection = document.getElementsByClassName("roleFT");
+var ftRoleArray = [].slice.call(ftRoleCollection);
+var ftArraycount = 1;
 var randomBallPosition = Math.floor(Math.random()*700 + 25);
 var balls = [];
 var bulletsPT = [];
@@ -28,7 +34,74 @@ window.onload = function() {
     selection.classList.add("hide");
     myGameArea.start();
   };
+  
+  //character selection buttons functionality
+  document.querySelector(".buttonFTL").onclick = function() {
+    if (ftArraycount === 0){
+      ftImgArray[ftArraycount].classList.add("hide");
+      ftRoleArray[ftArraycount].classList.add("hide");
+      ftArraycount = 2;
+      ftImgArray[ftArraycount].classList.remove("hide");
+      ftRoleArray[ftArraycount].classList.remove("hide");
+    }
+    else {
+      ftImgArray[ftArraycount].classList.add("hide");
+      ftRoleArray[ftArraycount].classList.add("hide");
+      ftArraycount--
+      ftImgArray[ftArraycount].classList.remove("hide");
+      ftRoleArray[ftArraycount].classList.remove("hide");
+    }
+  };
+  document.querySelector(".buttonFTR").onclick = function() {
+    if (ftArraycount === 2){
+      ftImgArray[ftArraycount].classList.add("hide");
+      ftRoleArray[ftArraycount].classList.add("hide");
+      ftArraycount = 0;
+      ftImgArray[ftArraycount].classList.remove("hide");
+      ftRoleArray[ftArraycount].classList.remove("hide");
+    }
+    else {
+      ftImgArray[ftArraycount].classList.add("hide");
+      ftRoleArray[ftArraycount].classList.add("hide");
+      ftArraycount++
+      ftImgArray[ftArraycount].classList.remove("hide");
+      ftRoleArray[ftArraycount].classList.remove("hide");
+    }
+  };
+  document.querySelector(".buttonPTL").onclick = function() {
+    if (ptArraycount === 0){
+      ptImgArray[ptArraycount].classList.add("hide");
+      ptRoleArray[ptArraycount].classList.add("hide");
+      ptArraycount = 2;
+      ptImgArray[ptArraycount].classList.remove("hide");
+      ptRoleArray[ptArraycount].classList.remove("hide");
+    }
+    else {
+      ptImgArray[ptArraycount].classList.add("hide");
+      ptRoleArray[ptArraycount].classList.add("hide");
+      ptArraycount--
+      ptImgArray[ptArraycount].classList.remove("hide");
+      ptRoleArray[ptArraycount].classList.remove("hide");
+    }
+  };
+  document.querySelector(".buttonPTR").onclick = function() {
+    if (ptArraycount === 2){
+      ptImgArray[ptArraycount].classList.add("hide");
+      ptRoleArray[ptArraycount].classList.add("hide");
+      ptArraycount = 0;
+      ptImgArray[ptArraycount].classList.remove("hide");
+      ptRoleArray[ptArraycount].classList.remove("hide");
+    }
+    else {
+      ptImgArray[ptArraycount].classList.add("hide");
+      ptRoleArray[ptArraycount].classList.add("hide");
+      ptArraycount++
+      ptImgArray[ptArraycount].classList.remove("hide");
+      ptRoleArray[ptArraycount].classList.remove("hide");
+    }
+  };
 
+  //change stage button functionality
   document.querySelector(".change-stage").onclick = function() {
     scoreBoard.classList.add("hide");
     var canvasSel = document.querySelector("canvas");
